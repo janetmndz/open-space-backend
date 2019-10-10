4d163d74b4d46655a94f7d3d6ec92651de7088bc
Janet Mendez <janetmendez.03@gmail.com>
2019-10-10 12:04:39 -0400

    fixed routes to create new users and makea token for them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
     # def index
     #     @users = User.all
-    #     render json: @users
     # end
     def show
         if (has_valid_token)
@@ -10,5 +9,23 @@ class UsersController < ApplicationController
         else
             render json: {message: "You don't got access to this"}, status: :unauthorized
         end
+    end
+
+    def create
+        new_user_params = user_params
+        user_password = params[:password]
+        new_user_params[:password] = user_password
+        @user = User.create(new_user_params)
+        if @user.valid?
+            render json: token(@user)
+        else
+            render json: {errors: @user.errors.full_message}
+        end
+
+    end
+    private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :topics, only: [:index]
   resources :notes, only: [:index]
   resources :posts, only: [:index]
-  resources :users, only: [:index, :show]
+  resources :users, only: [:show, :create]
   
   resources :tokens, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
