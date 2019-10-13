41bb6f4d6731e11ba4785e09ee37d6bb97e64d91
Janet Mendez <janetmendez.03@gmail.com>
2019-10-13 15:18:05 -0400

    made create and destroy for subs, need to fix topics

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,16 +1,28 @@
 class SubscriptionsController < ApplicationController
     
     def create
-        byebug
+        if(has_valid_token)  
+            @subscription = Subscription.create(subs_params)
+            render json: @subscription
+        else
+            render json: {message: "You don't have acess to this"}
+        end
     end
 
     def destroy
         if(has_valid_token)   
             @subscription =  Subscription.find(params[:id])
+            @topic = @subscription.topic
             @subscription.destroy()
-            render json: {message: "This subscription has been destroyed"}
+            render json: @topic
         else
             render json: {message: "You don't have acess to this"}
         end
+    end
+
+    private
+
+    def subs_params
+        params.require(:subscription).permit(:topic_id, :user_id)
     end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
     def index
         @topics = Topic.all
+        byebug
         render json: @topics
     end
     def show
