7816ebca881ab4da2308ebf7dc345e3a09e82951
Janet Mendez <janetmendez.03@gmail.com>
2019-10-14 18:12:16 -0400

    stopping off point for the day

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,30 +18,34 @@ class PostsController < ApplicationController
     end
 
     def update
-        @post = Post.find(params[:id])
-        @post.update(params.require(:post).permit(:content))
-        topics = params[:post][:topics]
-        postTopics = @post.topics.map{|pt| pt.id.to_s}
-
-        # When there are new topics added, make new Post Topics
-        if (!(topics - postTopics).empty?)
-            @newTopics = topics - postTopics
-            @newTopics.each{|nt| PostTopic.create(post_id: @post.id, topic_id: nt) }
-            # Get this agai because some things have changed
+        if (has_valid_token)
             @post = Post.find(params[:id])
-        end
+            @post.update(params.require(:post).permit(:content))
+            topics = params[:post][:topics]
+            postTopics = @post.topics.map{|pt| pt.id.to_s}
 
-        # When  there are topics removed, destroy Post Topics
-        if(!(postTopics - topics).empty?)
-            @removingTopics = postTopics - topics
-            @removingTopics.each do |pt|
-                pT = @post.post_topics.find_by(topic_id: pt)
-                pT.destroy()
+            # When there are new topics added, make new Post Topics
+            if (!(topics - postTopics).empty?)
+                @newTopics = topics - postTopics
+                @newTopics.each{|nt| PostTopic.create(post_id: @post.id, topic_id: nt) }
+                # Get this again because some things have changed
+                @post = Post.find(params[:id])
             end
-            # Get this agai because some things have changed
-            @post = Post.find(params[:id])
+
+            # When  there are topics removed, destroy Post Topics
+            if(!(postTopics - topics).empty?)
+                @removingTopics = postTopics - topics
+                @removingTopics.each do |pt|
+                    pT = @post.post_topics.find_by(topic_id: pt)
+                    pT.destroy()
+                end
+                # Get this again because some things have changed
+                @post = Post.find(params[:id])
+            end
+            
+            render json: @post
+        else
+            render json: {message: "You don't got access to this."}, status: :unauthorized
         end
-        
-        render json: @post
     end
 end
