d08ff9fa0140de1a62c3fe7d5cc1b3d0114128d9
Janet Mendez <janetmendez.03@gmail.com>
2019-10-24 13:52:37 -0400

    again, please work

--- config/initializers/ibm_watson_setup.rb
+++ config/initializers/ibm_watson_setup.rb
@@ -2,12 +2,12 @@ require "ibm_watson/authenticators"
 require "ibm_watson/tone_analyzer_v3"
 include IBMWatson
 
-APIKEY = ENV['ibm_watson_key']
-VERSION = ENV['ibm_watson_version']
-URL = ENV['ibm_watson_url']
-AUTHENTICATOR = Authenticators::IamAuthenticator.new(
-    apikey: APIKEY
-)
+# APIKEY = ENV['ibm_watson_key']
+# VERSION = ENV['ibm_watson_version']
+# URL = ENV['ibm_watson_url']
+# AUTHENTICATOR = Authenticators::IamAuthenticator.new(
+#     apikey: APIKEY
+# )
 # TONE_ANALYZER = ToneAnalyzerV3.new(
 #     version: VERSION,
 #     authenticator: authenticator
